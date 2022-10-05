6f90b3c78248698e3e62e9879f0c39529c65267c
CharlotteNoraJones <47127622+CharlotteNoraJones@users.noreply.github.com>
2022-10-04 21:26:15 -0500

    Made sure solution was saved in the correct directory.

---
+++ CS/TowerBuilder/TowerBuilder/Program.cs
@@ -0,0 +1,40 @@
+﻿//Build a pyramid-shaped tower, as an array/list of strings, 
+//given a positive integer number of floors. A tower block is represented with "*" character.
+
+using System;
+
+class Tower
+{
+    static public void Main(String[] args)
+    {
+        Console.WriteLine(GetRepeatedChar('*', 4));
+    }
+    public static string[] TowerBuilder(int nFloors)
+    {
+        string[] tower = { };
+        int towerWidth = 1 + ((nFloors - 1) * 2); 
+
+        for (int i = 0; i < nFloors; i++)
+        {
+            int blockWidth = 1 + ((i - 1) * 2);
+            int spaceWidth = nFloors - i;
+
+
+        }
+
+        return tower;
+    }
+
+    public static string GetRepeatedChar(char c, int num)
+    {
+        // Returns a string num amount of c
+        string new_str = "";
+        
+        for (int i = 0; i < num; i++)
+        {
+            new_str += c;
+        }
+        return new_str;
+    }
+
+}
