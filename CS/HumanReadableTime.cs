398ea4ed6267bfe5934300c58e693ce24af4ea78
CharlotteNoraJones <47127622+CharlotteNoraJones@users.noreply.github.com>
2023-01-20 14:22:27 -0600

    Create HumanReadableTime.cs

---
+++ CS/HumanReadableTime.cs
@@ -0,0 +1,28 @@
+/*
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a 
+human-readable format (HH:MM:SS)
+
+    HH = hours, padded to 2 digits, range: 00 - 99
+    MM = minutes, padded to 2 digits, range: 00 - 59
+    SS = seconds, padded to 2 digits, range: 00 - 59
+
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+*/
+
+
+
+static string GetReadableTime(int seconds)
+{
+    int hourLength = 3600;
+    int minLength = 60;
+
+    int hours = seconds / hourLength;
+    seconds -= hours * hourLength;
+
+    int mins = seconds / minLength;
+    seconds -= mins * minLength;
+
+    return ($"{hours.ToString("00")}:{mins.ToString("00")}:{seconds.ToString("00")}");
+}
